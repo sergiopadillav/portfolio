10b108a07d71eeb1e5e0d4e8228eb352c7c4b52a
Sergio Padilla Villaseñor <sergiolpv97@gmail.com>
2020-05-10 22:38:31 -0700

    Updates

---
+++ precache-manifest.53b04b53ca3793443837c830be550153.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3465ecd0af9e5d1ea857d1967690919",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "4bddad9508fc619cc920",
+    "url": "/portfolio/static/css/main.17c96f3b.chunk.css"
+  },
+  {
+    "revision": "d3a51ed187374089e7d0",
+    "url": "/portfolio/static/js/2.9e384841.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.9e384841.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4bddad9508fc619cc920",
+    "url": "/portfolio/static/js/main.4f7205bc.chunk.js"
+  },
+  {
+    "revision": "f619aeccc94a0d089131",
+    "url": "/portfolio/static/js/runtime-main.075c0788.js"
+  },
+  {
+    "revision": "5b07097315841a1f7a31451a3dd838a6",
+    "url": "/portfolio/static/media/img.5b070973.svg"
+  }
+]);
